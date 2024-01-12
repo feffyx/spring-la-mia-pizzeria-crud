0fa6a84bc14b05f0eacbc296d27b379ede06fc9e
feffyx <ziacofederica@gmail.com>
2024-01-12 13:33:20 +0100

    Initial commit

---
+++ src/main/java/org/learning/springlibrary/SpringLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.learning.springlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/learning/springlibrary/SpringLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.springlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
